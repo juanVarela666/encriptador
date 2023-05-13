a93dfe8e7f21d499ea3bfe0ad09f44ec7c28df7d
juanVarela666 <juanvc666@hotmail.com>
2023-05-13 17:00:38 -0600

    Primer commit

---
+++ encriptador.js
@@ -0,0 +1,58 @@
+let mensajeEncriptadoCopiado = false;
+
+function encriptar() {
+
+    let mensaje = document.getElementById("mensaje").value;
+    if (mensaje.trim() == "") {
+        return;
+    }
+    document.getElementById("caja1").style.display = "none";
+    document.getElementById("botonCopiar").style.display = "block";
+
+    mensaje = document.getElementById("mensaje").value;
+    mensajeEncriptado = mensaje.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat");
+
+    document.getElementById("mensaje2").innerHTML = mensajeEncriptado;
+}
+
+function desencriptar() {
+
+    if (!mensajeEncriptadoCopiado) {
+        return;
+    }
+
+    document.getElementById("caja1").style.visibility = "hidden";
+    document.getElementById("botonCopiar").style.display = "block";
+    
+    mensaje = document.getElementById("mensaje").value;
+    mensajeDesencriptado = mensaje.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u");
+
+    document.getElementById("mensaje2").innerHTML = mensajeDesencriptado;
+}
+
+function copiar() {
+
+    const botonCopiar = document.querySelector("#botonCopiar");
+    const cajaDeTexto = document.querySelector("#mensaje2");
+
+    botonCopiar.addEventListener('click', async () => {
+        const textoACopiar = cajaDeTexto.value;
+
+        cajaDeTexto.select();
+        
+        try {
+            await navigator.clipboard.writeText(textoACopiar);
+            botonCopiar.textContent = 'Texto copiado';
+            document.getElementById("mensaje").value = "";
+            mensajeEncriptadoCopiado = true;
+            setTimeout(() => {
+                botonCopiar.textContent = 'Copiar';
+            }, 2000);
+        } catch (error) {
+        }
+        });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    copiar();
+});
